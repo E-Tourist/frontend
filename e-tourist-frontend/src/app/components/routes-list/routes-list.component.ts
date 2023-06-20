4df56bf97830adfba83bd5a897dd43b812c5b4c2
irci <ircurlicus123@gmail.com>
2023-06-20 21:29:33 +0200

    Added routing to search component

--- e-tourist-frontend/src/app/components/routes-list/routes-list.component.ts
+++ e-tourist-frontend/src/app/components/routes-list/routes-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {RouteMinimumInfo} from "../../models/route-minimum-info";
 import {RoutesService} from "../../services/routes/routes.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-routes-list',
@@ -9,11 +10,21 @@ import {RoutesService} from "../../services/routes/routes.service";
 })
 export class RoutesListComponent {
   routesFound: RouteMinimumInfo[] = [];
+  placeId: number = 0;
 
-  constructor(private routesService: RoutesService) {
+  constructor(private routesService: RoutesService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.routesFound = this.routesService.getRoutesList();
+    this.route.params.subscribe(params => {
+      this.placeId = params['placeId'];
+    });
+
+    if (this.placeId > 0) {
+      this.routesFound = this.routesService.getRoutesListByPlace(this.placeId);
+    }
+    else {
+      this.routesFound = this.routesService.getRoutesList();
+    }
   }
 }
--- e-tourist-frontend/src/app/services/routes/routes.service.ts
+++ e-tourist-frontend/src/app/services/routes/routes.service.ts
@@ -7,48 +7,55 @@ import {RouteComments} from "../../models/route-comments";
   providedIn: 'root'
 })
 export class RoutesService {
+  mockRoutes: RouteMinimumInfo[] = [
+    {
+      id: 1,
+      name: "Magiczny Szlak Szczeciński",
+      mainImageUrl: "assets/routes-list/Waly_chrobrego.jpg",
+      distance: 10,
+      time: 2.5,
+      rating: 4.6,
+      isVerified: true
+    },
+    {
+      id: 2,
+      name: "Miejski Szlak Turystyczny Szczecin",
+      mainImageUrl: "assets/routes-list/miejski_szlak_turystyczny.jpg",
+      distance: 7,
+      time: 2,
+      rating: 4.2,
+      isVerified: true
+    },
+    {
+      id: 3,
+      name: "Łatwy szlak na Nosal - Zakopane",
+      mainImageUrl: "assets/routes-list/nosal.jpg",
+      distance: 1.8,
+      time: 1,
+      rating: 3.5,
+      isVerified: false
+    },
+    {
+      id: 4,
+      name: "Dookoła jeziora Głębokiego",
+      mainImageUrl: "assets/routes-list/glebokie.jpg",
+      distance: 5.2,
+      time: 1.1,
+      rating: 3,
+      isVerified: false
+    }
+  ];
 
   constructor() { }
 
   getRoutesList(): RouteMinimumInfo[] {
-    let routes: RouteMinimumInfo[] = [
-      {
-        id: 1,
-        name: "Magiczny Szlak Szczeciński",
-        mainImageUrl: "assets/routes-list/Waly_chrobrego.jpg",
-        distance: 10,
-        time: 2.5,
-        rating: 4.6,
-        isVerified: true
-      },
-      {
-        id: 2,
-        name: "Miejski Szlak Turystyczny Szczecin",
-        mainImageUrl: "assets/routes-list/miejski_szlak_turystyczny.jpg",
-        distance: 7,
-        time: 2,
-        rating: 4.2,
-        isVerified: true
-      },
-      {
-        id: 3,
-        name: "Łatwy szlak na Nosal - Zakopane",
-        mainImageUrl: "assets/routes-list/nosal.jpg",
-        distance: 1.8,
-        time: 1,
-        rating: 3.5,
-        isVerified: false
-      },
-      {
-        id: 4,
-        name: "Dookoła jeziora Głębokiego",
-        mainImageUrl: "assets/routes-list/glebokie.jpg",
-        distance: 5.2,
-        time: 1.1,
-        rating: 3,
-        isVerified: false
-      }
-    ];
+    let routes: RouteMinimumInfo[] = this.mockRoutes;
+
+    return routes;
+  }
+
+  getRoutesListByPlace(placeId: number): RouteMinimumInfo[] {
+    let routes: RouteMinimumInfo[] = this.mockRoutes;
 
     return routes;
   }
